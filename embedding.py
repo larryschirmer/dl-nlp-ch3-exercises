e24bce275174758de5e6caa3d591ce0b044f72a9
Larry Schirmer <larryschirmer@gmail.com>
2019-12-29 21:39:06 -0500

    generate labeled embedding file

--- embedding.py
+++ embedding.py
@@ -1,51 +1,35 @@
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Embedding
-from utils import tsne_plot
+from keras.layers import Embedding, Flatten, Dense, Dropout
+from utils import process_training_data, process_test_data, save_embedding
 
-docs = ["Chuck Berry rolled over everyone who came before him ? and turned up everyone who came after. We'll miss you",
-        "Help protect the progress we've made in helping millions of Americans get covered.",
-        "Let's leave our children and grandchildren a planet that's healthier than the one we have today.",
-        "The American people are waiting for Senate leaders to do their jobs.",
-        "We must take bold steps now ? climate change is already impacting millions of people.",
-        "Don't forget to watch Larry King tonight",
-        "Ivanka is now on Twitter - You can follow her",
-        "Last night Melania and I attended the Skating with the Stars Gala at Wollman Rink in Central Park",
-        "People who have the ability to work should. But with the government happy to send checks",
-        "I will be signing copies of my new book"
-        ]
+# constants
+max_len = 100
+embedding_size = 100
+loss = 'binary_crossentropy'
+optimizer = 'adam'
+metrics = ['accuracy']
+train_filename = 'yelp_reviews_train.tsv'
+test_filename = 'yelp_reviews_test.tsv'
 
-docs = [d.lower() for d in docs]
+# pre-process data
+data, labels, vocab = process_training_data(train_filename, max_len)
+test_data, test_labels = process_test_data(test_filename, vocab, max_len)
 
-count_vec = CountVectorizer().fit(docs)
-tokenizer = count_vec.build_tokenizer()
-
-input_array = []
-for doc in docs:
-    x = []
-    for token in tokenizer(doc):
-        x.append(count_vec.vocabulary_.get(token))
-
-    input_array.append(x)
-
-max_len = max([len(d) for d in input_array])
-vocab_size = len(count_vec.vocabulary_)
-input_array = pad_sequences(input_array, maxlen=max_len, padding='post')
-
+# generate model
+vocab_size = len(vocab)
 
 model = Sequential()
-model.add(Embedding(vocab_size, 8, input_length=max_len))
-model.compile('rmsprop', 'mse')
+embedding = Embedding(vocab_size, embedding_size, input_length=max_len)
+model.add(embedding)
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
 
-output_array = model.predict(input_array)
+model.summary()
+model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+model.fit(data, labels, epochs=100, verbose=1, batch_size=32,
+          shuffle=True, validation_data=(test_data, test_labels))
 
-M = {}
-for sec_index in range(len(input_array)):
-    for word_index in range(len(input_array[sec_index])):
-        token = input_array[sec_index][word_index]
-        word = count_vec.get_feature_names()[token]
-        M[word] = output_array[sec_index][word_index]
-
-tsne_plot(M, max_words=vocab_size)
+save_embedding('embedding_labeled.txt', embedding.get_weights()[0], vocab)
--- utils.py
+++ utils.py
@@ -36,3 +36,91 @@ def tsne_plot(model, max_words=100, figure_name='tsne'):
                      va='bottom')
 
     plt.savefig(figure_name+'.png', bbox_inches='tight')
+
+
+def save_embedding(outputFile, weights, vocabulary):
+    rev = {v: k for k, v in vocabulary.items()}
+
+    with codecs.open(outputFile, "w") as f:
+        f.write(str(len(vocabulary)) + ' ' + str(weights.shape[1]) + "\n")
+
+        for word_index, word in sorted(iter(rev.items())):
+            f.write(word + " ")
+
+            for weight_index in range(len(weights[word_index])):
+                f.write(str(weights[word_index][weight_index]) + " ")
+
+            f.write("\n")
+
+
+def getLines(f):
+    lines = [line.rstrip() for line in open(f)]
+    return lines
+
+
+def create_vocabulary(vocabulary, sentences):
+    vocabulary["<unk>"] = 0
+
+    for sentence in sentences:
+        for word in sentence.strip().split():
+            word = re.sub("[.,:;'\"!?()]+", "", word.lower())
+            if word not in vocabulary:
+                vocabulary[word] = len(vocabulary)
+
+
+def process_training_data(textFile, max_len):
+    data = []
+    sentences = getLines(textFile)
+    vocab = dict()
+    labels = []
+    create_vocabulary(vocab, sentences)
+
+    for s in sentences:
+        words = []
+        m = re.match("^([^\t]+)\t(.+)$", s.rstrip())
+
+        if m:
+            sentence = m.group(1)
+            labels.append(int(m.group(2)))
+
+        for word in sentence.split(" "):
+            word = re.sub("[.,:;'\"!?()]+", "", word.lower())
+
+            if word != '':
+                words.append(vocab[word])
+
+        data.append(words)
+
+    data = pad_sequences(data, maxlen=max_len, padding='post')
+
+    return data, labels, vocab
+
+
+def process_test_data(textFile, vocab, max_len):
+    data = []
+    sentences = getLines(textFile)
+    labels = []
+    create_vocabulary(vocab, sentences)
+
+    for s in sentences:
+        words = []
+        m = re.match("^([^\t]+)\t(.+)$", s.rstrip())
+
+        if m:
+            sentence = m.group(1)
+            labels.append(int(m.group(2)))
+
+        for word in sentence.split(" "):
+            word = re.sub("[.,:;'\"!?()]+", "", word.lower())
+
+            if word != '':
+                if word in vocab:
+                    words.append(vocab[word])
+                else:
+                    words.append(vocab["<unk>"])
+
+        data.append(words)
+
+    data = pad_sequences(data, maxlen=max_len, padding='post')
+
+    return data, labels
